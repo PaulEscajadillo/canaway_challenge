1c536dbe36000a92fa7036c8624781593b9151bf
PaulEscajadillo <78814585+PaulEscajadillo@users.noreply.github.com>
2023-08-08 01:34:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,39 @@ import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 
-labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
-sizes = [15, 30, 45, 10]
+from streamlit_echarts import st_echarts
+def render_pie_simple():
+    options = {
+        "title": {"text": "某站点用户访问来源", "subtext": "纯属虚构", "left": "center"},
+        "tooltip": {"trigger": "item"},
+        "legend": {"orient": "vertical", "left": "left",},
+        "series": [
+            {
+                "name": "访问来源",
+                "type": "pie",
+                "radius": "50%",
+                "data": [
+                    {"value": 1048, "name": "搜索引擎"},
+                    {"value": 735, "name": "直接访问"},
+                    {"value": 580, "name": "邮件营销"},
+                    {"value": 484, "name": "联盟广告"},
+                    {"value": 300, "name": "视频广告"},
+                ],
+                "emphasis": {
+                    "itemStyle": {
+                        "shadowBlur": 10,
+                        "shadowOffsetX": 0,
+                        "shadowColor": "rgba(0, 0, 0, 0.5)",
+                    }
+                },
+            }
+        ],
+    }
+    st_echarts(
+        options=options, height="600px",
+    )
+render_pie_simple()
 
-fig, ax = plt.subplots()
-ax.pie(sizes, labels=labels)
 
 st.header("Ventas")
 
